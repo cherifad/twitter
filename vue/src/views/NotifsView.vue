9e5ab1c2f69ba26a4fc1adadd1fa1fd4184a19fe
JulesSs05 <jules.silvestri@etu.univ-smb.fr>
2023-03-23 18:27:14 +0100

    commit notifs views

--- vue/src/views/NotifsView.vue
+++ vue/src/views/NotifsView.vue
@@ -1,7 +1,25 @@
 <template>
-<h1>Notifications</h1>
+    <div class="relative">
+        <h1 class="flex relative font-extrabold text-xl top-3 left-5">Notifications</h1>
+        <IconSettings class="absolute top-5 right-5"/>
+        <TabGroup>
+            <TabList class="flex mt-5">
+                <Tab class="flex-1 font-bold text-xm top-3 text-zinc-700 py-4 hover:bg-zinc-800">All</Tab>
+                <Tab class="flex-1 font-bold text-xm top-3 text-zinc-700 py-4 hover:bg-zinc-800">Verfied</Tab>
+                <Tab class="flex-1 font-bold text-xm top-3 text-zinc-700 py-4 hover:bg-zinc-800">Mentions</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>content 1</TabPanel>
+                <TabPanel>content 2</TabPanel>
+                <TabPanel>content 3</TabPanel>
+            </TabPanels>
+        </TabGroup>
+    </div>
 </template>
 
 <script setup>
+import { TabGroup, TabList, TabPanel, TabPanels, Tab } from '@headlessui/vue';
+
+import IconSettings from '../components/icons/IconSettings.vue'; 
 
 </script>
